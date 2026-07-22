d0687dc7c61782dbf3c3e2d9280607b146f7dace
Isidro1234 <skillsizy@gmail.com>
2026-07-22 16:01:27 -0500

    novos

--- app/page.tsx
+++ app/page.tsx
@@ -245,7 +245,7 @@ export default function Home() {
           ref={delText.ref}
           className={cls("reveal-up", delText.inView && "is-visible")}
           color={'white'} marginBottom={10} fontSize={12}
-        >Encontre qualquer enderecos de provincias angolanas  </Text>
+        >Encontre qualquer enderecos de angola  </Text>
         <Box
           ref={delImage.ref}
           className={cls("reveal-scale", delImage.inView && "is-visible")}
